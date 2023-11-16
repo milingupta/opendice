fc3cfcf9729767ca8053b53e08a76d8e5e2126a4
Milin Gupta <milingupta15@gmail.com>
2023-11-15 22:36:14 -0500

    Added placeholder for empty history screens

--- SoftDice/View Controllers/History/TwoHistoryVC.swift
+++ SoftDice/View Controllers/History/TwoHistoryVC.swift
@@ -10,6 +10,14 @@ import UIKit
 class TwoHistoryVC: UITableViewController {
 
     var rollHistory = RollHistory.shared
+    
+    private let placeholderView = UIView()
+    private let placeholderLabel = UILabel()
+    private let placeholderImageView = UIImageView()
+    
+    private lazy var sortButton: UIBarButtonItem = {
+        return UIBarButtonItem(image: UIImage(systemName: "arrow.up.arrow.down"), style: .plain, target: self, action: #selector(sortTapped))
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -17,7 +25,9 @@ class TwoHistoryVC: UITableViewController {
         tableView.register(TwoHistoryCell.self, forCellReuseIdentifier: "cell")
         tableView.rowHeight = 80
         
-        let sortButton = UIBarButtonItem(image: UIImage(systemName: "arrow.up.arrow.down"), style: .plain, target: self, action: #selector(sortTapped))
+        configurePlaceholderView()
+        updatePlaceholderVisibility()
+        
         navigationItem.rightBarButtonItem = sortButton
     }
     
@@ -39,9 +49,55 @@ class TwoHistoryVC: UITableViewController {
 
     @objc func sortTapped() {
         rollHistory.reverseTwoRollHistory()
-        tableView.reloadData()
+        UIView.transition(with: tableView, duration: 0.35, options: .transitionCrossDissolve, animations: { self.tableView.reloadData() })
+    }
+    
+    private func configurePlaceholderView() {
+        placeholderView.translatesAutoresizingMaskIntoConstraints = false
+        placeholderLabel.translatesAutoresizingMaskIntoConstraints = false
+        placeholderImageView.translatesAutoresizingMaskIntoConstraints = false
         
-        UIView.transition(with: tableView, duration: 0.35, options: .transitionFlipFromTop, animations: { self.tableView.reloadData() })
+        placeholderLabel.text = "No rolls"
+        placeholderLabel.font = UIFont.preferredFont(forTextStyle: .headline)
+        placeholderLabel.textColor = .placeholderText
+        
+        let imageSize: CGFloat = 80
+        
+        placeholderImageView.image = UIImage(systemName: "dice.fill")
+        placeholderImageView.tintColor = .placeholderText
+        placeholderImageView.contentMode = .scaleAspectFit
+        
+        placeholderView.addSubview(placeholderImageView)
+        placeholderView.addSubview(placeholderLabel)
+        
+        view.addSubview(placeholderView)
+        
+        // Calculate a shift value that visually centers the placeholder
+        let shiftUpwards: CGFloat = 100 // Adjust this value as needed
+        
+        NSLayoutConstraint.activate([
+            placeholderView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            placeholderView.centerYAnchor.constraint(equalTo: view.centerYAnchor, constant: -shiftUpwards),
+            placeholderImageView.centerXAnchor.constraint(equalTo: placeholderView.centerXAnchor),
+            placeholderImageView.centerYAnchor.constraint(equalTo: placeholderView.centerYAnchor),
+            placeholderImageView.widthAnchor.constraint(equalToConstant: imageSize),
+            placeholderImageView.heightAnchor.constraint(equalToConstant: imageSize),
+            placeholderLabel.topAnchor.constraint(equalTo: placeholderImageView.bottomAnchor, constant: 12),
+            placeholderLabel.centerXAnchor.constraint(equalTo: placeholderView.centerXAnchor)
+        ])
+    }
+    
+    private func updatePlaceholderVisibility() {
+        let rollCount = rollHistory.twoRollHistory.count
+
+        // Show placeholder view only when there are no rolls
+        placeholderView.isHidden = rollCount > 0
+
+        // Enable sort button only when there are two or more rolls
+        sortButton.isEnabled = rollCount >= 2
+
+        // Enable or disable scrolling based on the presence of rolls
+        tableView.isScrollEnabled = rollCount > 0
     }
     
 }
